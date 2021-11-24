41001c5c3e9774c91d4171c357094c8d0e235a81
Sanou <sanouarouna90@gmail.com>
2021-11-24 00:14:56 +0000

    add target folder

---
+++ target/classes/static/static/js/91.0b25f650.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[91],{851:function(e,c){}}]);
+//# sourceMappingURL=91.0b25f650.chunk.js.map
