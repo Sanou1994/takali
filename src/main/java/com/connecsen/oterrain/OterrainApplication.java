426822ed6bb73725f2f0f55206069e53b33c1f9e
Sanou <sanouarouna90@gmail.com>
2021-12-01 07:51:14 +0000

    stop job

--- src/main/java/com/connecsen/oterrain/OterrainApplication.java
+++ src/main/java/com/connecsen/oterrain/OterrainApplication.java
@@ -46,11 +46,11 @@ public class OterrainApplication implements CommandLineRunner {
 	}
     
 	
-	
-	  @PostConstruct
-	  public void scheduleRunnableWithCronTrigger() {
-	  taskScheduler.scheduleWithFixedDelay(new JobRunner(iReservationService,terrainRepository),1); }
-	 
+	/*
+	 * @PostConstruct public void scheduleRunnableWithCronTrigger() {
+	 * taskScheduler.scheduleWithFixedDelay(new
+	 * JobRunner(iReservationService,terrainRepository),1); }
+	 */
 	 
 	 
 	@Override  
