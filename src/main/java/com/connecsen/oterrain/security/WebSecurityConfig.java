7ba3ca20f424737404ca62c63ec01976fa44b4a2
Sanou <sanouarouna90@gmail.com>
2021-12-07 06:48:05 +0000

    ok finance

--- src/main/java/com/connecsen/oterrain/security/WebSecurityConfig.java
+++ src/main/java/com/connecsen/oterrain/security/WebSecurityConfig.java
@@ -48,7 +48,7 @@ private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	// We don't need CSRF for this example
 	httpSecurity.csrf().disable()
 	
-	.authorizeRequests().antMatchers("/user/add/transaction/**","/static/**","/static/favicon.ico","/static/static/**","/static/favicon-o-terrain.png/**","/static/avatars**","/assets/**","/","/#","/#/","/#/accueil-visiteur","/match/**","/terrain/**","/tournoi/**","/v3/api-docs/**","/swagger-ui/**","/acceuil/**","/user/role/**","/user/register/**","/user/login/**","/user/forgot").permitAll().
+	.authorizeRequests().antMatchers("/**","/user/add/transaction/**","/static/**","/static/favicon.ico","/static/static/**","/static/favicon-o-terrain.png/**","/static/avatars**","/assets/**","/","/#","/#/","/#/accueil-visiteur","/match/**","/terrain/**","/tournoi/**","/v3/api-docs/**","/swagger-ui/**","/acceuil/**","/user/role/**","/user/register/**","/user/login/**","/user/forgot").permitAll().
 
 	// all other requests need to be authenticated
 	anyRequest().authenticated().and().
